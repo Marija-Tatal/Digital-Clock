79a8f20c94733c7b7d7e9cf357ddc16e9da02312
Marija Tatalovic <marija.tatalovic@yahoo.com>
2023-01-20 13:24:42 +0100

    Digital-Clock

---
+++ src/DigitalClock1/DigitalClock.java
@@ -0,0 +1,15 @@
+package DigitalClock1;
+
+public class DigitalClock {
+    public static void main(String[] args) {
+        for (int i = 0; i <=24 ; i++) {
+            for (int j = 0; j <=59 ; j++) {
+                System.out.println(i+":"+j);
+            }
+            System.out.println();
+            
+        }
+
+
+    }
+}
